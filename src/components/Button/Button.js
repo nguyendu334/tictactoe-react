0c99638a6680fa8a87f1038c7356efa00bfa0580
_nguyen.du <1851061329@e.tlu.edu.vn>
2023-02-22 17:22:30 +0700

    Add scss

---
+++ src/components/Button/Button.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Button.scss'
+
+export default function Button(props) {
+  return (
+    <button className={`button ${props.className || ''}`}>{props.children}</button>
+  )
+}
