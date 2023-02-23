d9dbe7c62b1253759237fb2f3beb663ce8c11f58
_nguyen.du <1851061329@e.tlu.edu.vn>
2023-02-23 10:52:34 +0700

    Add props in card

---
+++ src/components/card/Card.js
@@ -0,0 +1,119 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+const StyledCard = styled.div`
+    position: relative;
+`;
+const CardImage = styled.div`
+    height: 400px;
+    width: 100%;
+    border-radius: 8px;
+`;
+const CardImg = styled.img`
+    display: block;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    border-radius: inherit;
+`;
+const CardContent = styled.div`
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, 50%);
+    width: calc(100% - 36px);
+    bottom: 0;
+    background-color: white;
+    z-index: 10;
+    border-radius: 20px;
+    padding: 20px;
+`;
+
+const CardTop = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 30px;
+`;
+
+const CardUser = styled.div`
+    display: flex;
+    align-items: center;
+    column-gap: 12px;
+`;
+
+const UserAvatar = styled.img`
+    width: 30px;
+    height: 30px;
+    border-radius: 100rem;
+    object-fit: cover;
+    flex-shrink: 0;
+`;
+const UserName = styled.span`
+    font-weight: 300;
+    font-size: 16px;
+    color: #333;
+`;
+const CardFooter = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+const CardTitle = styled.h3`
+    font-size: 18px;
+    font-weight: 500;
+    color: ${props => props.theme.colors.blue};
+`;
+const CardAmount = styled.span`
+    font-size: ${(props) => props.fontSize || '18px'};
+    font-weight: bold;
+    ${(props) =>
+        props.secondary &&
+        css`
+            background: linear-gradient(86.88deg, #20e3b2, #2cccff);
+        `};
+    ${(props) =>
+        !props.secondary &&
+        css`
+            background: linear-gradient(86.88deg, #7d6aff 1.38%, #ffb86c 64.35%, #fc2872 119.91%);
+        `};
+    color: transparent;
+    -webkit-background-clip: text;
+    background-clip: text;
+`;
+const CardMeta = styled.div`
+    display: flex;
+    align-items: center;
+    column-gap: 12px;
+`;
+
+export default function Card(props) {
+    return (
+        <StyledCard>
+            <CardImage>
+                <CardImg
+                    src="https://cdn.dribbble.com/users/2400293/screenshots/15883991/media/c5a6d6098f4ec6a0ce1b38909d82a494.png?compress=1&resize=1000x750&vertical=top"
+                    alt=""
+                />
+            </CardImage>
+            <CardContent>
+                <CardTop>
+                    <CardUser>
+                        <UserAvatar
+                            src="https://cdn.dribbble.com/users/2400293/screenshots/15883991/media/c5a6d6098f4ec6a0ce1b38909d82a494.png?compress=1&resize=1000x750&vertical=top"
+                            alt=""
+                        />
+                        <UserName>@zndrson</UserName>
+                    </CardUser>
+                    <CardMeta>
+                        <img src="/heart.svg" alt="heart" />
+                        <span>256</span>
+                    </CardMeta>
+                </CardTop>
+                <CardFooter>
+                    <CardTitle>Cosmic Perspective</CardTitle>
+                    <CardAmount secondary={props.secondary}>12,000 PSL</CardAmount>
+                </CardFooter>
+            </CardContent>
+        </StyledCard>
+    );
+}
---
+++ src/components/card/CardList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledCardList = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 90px 30px;
+    padding: 30px;
+`;
+
+export default function CardList(props) {
+    return <StyledCardList>{props.children}</StyledCardList>;
+}
