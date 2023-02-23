39aae5e5b9dda8ce7d4eb4050789ca330c242920
_nguyen.du <1851061329@e.tlu.edu.vn>
2023-02-23 13:03:19 +0700

    Use Tailwind css in project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import './App.css';
-import Game from './components/tictactoe/Game';
+// import Card from './components/card/Card';
+// import Game from './components/tictactoe/Game';
+import CardList from './components/card/CardList';
+import { ThemeProvider } from 'styled-components';
+import CardTailwind from './components/card/CardTailwind';
+
+const theme = {
+    colors: {
+        blue: '#2979ff',
+    },
+};
 
 function App() {
     return (
-        <div>
-            <Game></Game>
-        </div>
+        <ThemeProvider theme={theme}>
+            <CardList>
+                {/* <Card /> */}
+                <CardTailwind />
+                <CardTailwind primary fontSize="text-2xl"/>
+                <CardTailwind />
+                {/* <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card /> */}
+            </CardList>
+        </ThemeProvider>
     );
 }
 
